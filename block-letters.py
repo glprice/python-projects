68fdb9aa0942dfe137218558965d8c7bccebc0c9
glprice <glewisprice2017@gmail.com>
2025-08-16 12:30:57 -0400

    Create block-letters.py

---
+++ block-letters.py
@@ -0,0 +1,12 @@
+# G--- P---
+# I love Pumpkin Pie
+
+print("""
+   G G G     P P P P
+ G       G   P       P
+ G           P       P
+ G   G G G   P P P P
+ G       G   P
+ G       G   P
+   G G G     P
+  """)

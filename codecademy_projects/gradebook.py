eb670f274f38da490671ad3c4c4461cf74e8c2ec
Gage L Price <glewisprice2017@gmail.com>
2025-08-28 22:17:15 -0400

    Added "gradebook.py" to "codecademy_projects"

---
+++ codecademy_projects/gradebook.py
@@ -0,0 +1,35 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+
+# list of subjects taken and list of corresponing grades in order
+subjects = ["physics","calculus", "poetry", "history"]
+grades = [98, 97, 85, 88]
+
+# combined list of "subjects" and "grades"
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+# prints gradebook in terminal
+print("Semester Final Scores: \n" + str(gradebook))
+
+# adds "computer science" and the corresponding grade to the lists
+subjects.append("computer science")
+grades.append(100)
+gradebook.append(["computer science", 100])
+
+# adds "visual arts" and the corresponding grade to the lists
+subjects.append("visual arts")
+grades.append(93)
+gradebook.append(["visual arts", 93])
+
+# modifies grade of visual arts in "grades" and "gradebook"
+grades[-1]= 98
+gradebook[-1][1] = 98
+
+# removes numerical grade from list and then replaces it using .remove and .append (as per instructions)
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+
+# combines" gradebook" and "last_semster_gradebook" into one list in order 
+full_gradebook = last_semester_gradebook + gradebook 
+print("\n End of Year Results:\n" + str(full_gradebook))
